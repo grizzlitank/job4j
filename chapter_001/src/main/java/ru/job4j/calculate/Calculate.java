e8754d673835ad3cb5a7bf814a20b788399c617c
grizzlitank <mailto:ilya.arkhipov.89@mail.ru>
2018-09-07 22:58:40 +0300

    1.3. JavaDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,8 @@
 package ru.job4j.calculate;
-
+/**
+*Calculate
+*@aughtor Ilya Arkhipov (ilya.arkhipov.89@mail.ru)
+*/
 public class Calculate{
 	public static void main(String[] args){
 		System.out.println("Hello world");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Ilya Arkhipov (mailto:ilya.arkhipov.89@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
