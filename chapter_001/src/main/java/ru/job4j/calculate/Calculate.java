421c4533316e6047e68907ac492ae61dfafa783f
grizzlitank <mailto:ilya.arkhipov.89@mail.ru>
2018-09-12 23:43:17 +0300

    2.5. Инспектор кода IDEA. [#69757]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,19 +1,20 @@
 package ru.job4j.calculate;
 /**
-*Calculate
-*@author Ilya Arkhipov (ilya.arkhipov.89@mail.ru)
-*/
+ *Calculate
+ *@author Ilya Arkhipov (ilya.arkhipov.89@mail.ru)
+ */
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
-	
+
 	/**
-     * Method echo.
-     * @param name Your name.
-     * @return Echo plus your name.
-     */
-    public String echo(String name) {
-        return "Echo, echo, echo : " + name;
-    }
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	public String echo(String name) {
+
+		return "Echo, echo, echo : " + name;
+	}
 }
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,25 +5,25 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Arkhipov Ilya (mailto:ilya.arkhipov.89@mail.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Arkhipov Ilya (mailto:ilya.arkhipov.89@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 
 public class CalculateTest {
- /**
-* Test echo.
-*/
+	/**
+	 * Test echo.
+	 */
 
-@Test
-public void whenTakeNameThenTreePlusName() {
-	String input = "Arkhipov Ilya";
-	String expect = "Echo, echo, echo : Arkhipov Ilya";
-	Calculate calc = new Calculate();
-	String result = calc.echo(input);
-	assertThat(result, is(expect));
-}
+	@Test
+	public void whenTakeNameThenTreePlusName() {
+		String input = "Arkhipov Ilya";
+		String expect = "Echo, echo, echo : Arkhipov Ilya";
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
 
 }
