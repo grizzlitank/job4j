58c686593192cdfa0b5fdd0faf763fa7dd91760c
grizzlitank <mailto:ilya.arkhipov.89@mail.ru>
2018-10-11 22:55:17 +0300

    Задача к контрольным вопросам. Два сортированных массива.

---
+++ chapter_001/src/main/java/ru/job4j/array/TwoArrays.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+public class TwoArrays {
+
+    public int[] getSortArray(int[] arrayX, int[] arrayY) {
+        int counterX = 0;
+        int counterY = 0;
+        int counterSum = 0;
+        int[] arraySum = new int[arrayX.length + arrayY.length];
+        while (counterX < arrayX.length || counterY < arrayY.length) {
+            if (counterX < arrayX.length && arrayX[counterX] <= arrayY[counterY]) {
+                arraySum[counterSum] = arrayX[counterX];
+                counterSum++;
+                counterX++;
+            } else if (counterX == arrayX.length || arrayX[counterX] > arrayY[counterY]) {
+                arraySum[counterSum] = arrayY[counterY];
+                counterSum++;
+                counterY++;
+            }
+        }
+        return arraySum;
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/TwoArraysTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TwoArraysTest {
+    @Test
+    public void whenStartWithTwoArrays() {
+        TwoArrays twoArrays = new TwoArrays();
+        int[] arrayX = new int[]{1, 2, 4};
+        int[] arrayY = new int[]{3, 5, 6, 10};
+        int[] result = twoArrays.getSortArray(arrayX, arrayY);
+        int[] expect = new int[]{1, 2, 3, 4, 5, 6, 10};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenStartWithTwoIdenticalArrays() {
+        TwoArrays twoArrays = new TwoArrays();
+        int[] arrayX = new int[]{1, 2, 3};
+        int[] arrayY = new int[]{1, 2, 3};
+        int[] result = twoArrays.getSortArray(arrayX, arrayY);
+        int[] expect = new int[]{1, 1, 2, 2, 3, 3};
+        assertThat(result, is(expect));
+    }
+
+
+}
