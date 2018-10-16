d33fcc5b0b26745defbe31d3db768772991cc065
grizzlitank <mailto:ilya.arkhipov.89@mail.ru>
2018-10-16 22:12:50 +0300

    Professions

---
+++ chapter_002/src/main/java/ru/job4j/professions/Profession.java
@@ -0,0 +1,27 @@
+package ru.job4j.professions;
+
+public class Profession {
+    private String name;
+    private String profession;
+
+    public Profession(String name, String profession) {
+        this.name = name;
+        this.profession = profession;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/professions/Student.java
@@ -0,0 +1,18 @@
+package ru.job4j.professions;
+
+public class Student {
+
+    String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
