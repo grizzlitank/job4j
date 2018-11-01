f47ce062950d3a3d11869310fd89856146d9acdc
grizzlitank <mailto:ilya.arkhipov.89@mail.ru>
2018-11-02 00:26:42 +0300

    4. Используя шаблон проектирования

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
@@ -0,0 +1,18 @@
+package ru.job4j.pseudo;
+
+/**
+ * @author Ilya Arkhipov
+ * @version $Id$
+ * @since 0.1
+ */
+public class Triangle implements Shape{
+    @Override
+    public String draw() {
+        StringBuilder pic = new StringBuilder();
+        pic.append("+");
+        pic.append("++");
+        pic.append("+++");
+        pic.append("++++");
+        return pic.toString();
+    }
+}
